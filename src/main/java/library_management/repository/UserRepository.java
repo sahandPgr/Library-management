578abfe31f19730089e0db34ef85bd287a4ed9c6
sahand.ah <sahandmac@sahandMacs-MacBook-Pro.local>
2026-07-16 12:35:13 +0330

    feat: implement custom user details service

--- src/main/java/library_management/repository/UserRepository.java
+++ src/main/java/library_management/repository/UserRepository.java
@@ -3,7 +3,7 @@ package library_management.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import library_management.entity.User;
-
+import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
-
+    Optional<User> findByEmail(String email);
 }
---
+++ src/main/java/library_management/service/CustomUserDetailsService.java
@@ -0,0 +1,43 @@
+package library_management.service;
+
+import java.util.List;
+
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import library_management.entity.User;
+import library_management.repository.UserRepository;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email)
+            throws UsernameNotFoundException {
+
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("User not found"));
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getEmail())
+                .password(user.getPassword())
+                .authorities(
+                        List.of(
+                                new SimpleGrantedAuthority(
+                                        "ROLE_" + user.getRole().name()
+                                )
+                        )
+                )
+                .build();
+    }
+}
